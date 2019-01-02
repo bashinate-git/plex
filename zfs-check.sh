16ab5f0483dd23f21fe00a757e937ba2810d9201
root <root@winterfell.bashinate.com>
2019-01-01 19:32:25 -0600

    first commit

---
+++ zfs-check.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+EMAIL_ADD=5124618144@mms.cricketwireless.net
+
+checkstatus=$(zpool status -x | grep 'all pools are healthy')
+
+if [ "$checkstatus" != 'all pools are healthy' ]; then
+    /bin/date > /tmp/zfs.stat
+    echo >> /tmp/zfs.stat
+    /bin/hostname >> /tmp/zfs.stat
+    echo >> /tmp/zfs.stat
+    /sbin/zpool status -x >> /tmp/zfs.stat
+    cat /tmp/zfs.stat | /usr/bin/mail -s "Disk failure in server : `hostname`" $EMAIL_ADD
+fi
